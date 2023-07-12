a796e5484b256c18a15da4f475418199df4fd1e8
Medicine Zhong <MedicineZhong@outlook.com>
2023-07-12 18:06:54 +0800

    update

--- src/GraphDrawing.py
+++ src/GraphDrawing.py
@@ -1,7 +1,8 @@
-from Network import Network
-from type import NodePosSet, Pos, V1, V2
+from Network import Network, EdgeSet, NodeSet
+from type import NodePosSet, V1, V2
 from typing import List
 from OctTree import OctTree
+from Pos import Pos
 import time
 import math
 import random
@@ -123,3 +124,56 @@ class GraphDrawing:
     
     def cal_energy(self, network: Network, tree: OctTree, a: float, r: float, nodePoses: NodePosSet) -> float:
         return self.cal_attractive_energy(network.getEdgeSet(), a, nodePoses) - self.cal_repulsive_energy(network, tree, r, nodePoses)
+    
+    def cal_enrgy_core(self, pu: Pos, pv: Pos, p: float) -> float:
+        dst = (pu - pv).length()
+        if abs(dst) < 1e-6:
+            return 0.0
+        return math.log(dst) if abs(p) < 1e-6 else  math.pow(dst, p) / p #todo
+    
+    
+    def cal_enrgy_core_tree(self, pu: Pos, tree: OctTree, p: float) -> float:
+        dist = (tree.massCenter - pu).length()
+        if(abs(dist) < 1e-6):
+            return 0.0
+        
+        if(not tree.isLeaf and dist < 1.0 * tree.width()):
+            energy = 0.0
+            for nd in tree.data:
+                if (nd.tr is not None):
+                    energy += self.cal_enrgy_core_tree(pu, nd.tr, p)
+            return energy
+        
+        return tree.weight * self.cal_enrgy_core(pu, tree.massCenter, p)
+    
+    def cal_attractive_energy(self, edges :EdgeSet, a: float, nodePoses: NodePosSet) -> float:
+        energy = 0.0
+        for edge in edges:
+            u, v = V1(edge), V2(edge)
+            energy += 2 * self.cal_enrgy_core(nodePoses[u], nodePoses[v], a)
+        return energy
+    
+    def cal_repulsive_energy(self, network: Network, tree: OctTree, r: float, nodePoses: NodePosSet) -> float:
+        
+        nodes: NodeSet = network.getNodeSet()
+        energy = 0.0
+        for u in nodes:
+            f = network.getFactor(u)
+            energy += f * self.cal_enrgy_core_tree(nodePoses[u], tree, r)
+            
+        return energy
+    
+    
+    def update_param(self, iter: int):
+        if iter >= 50 and self.fr < 1.0:
+            self.a = self.fa
+            self.r = self.fr
+            
+            if iter <= 0.6 * self.nIters:
+                self.a = self.a + 1.1 * (1.0 - self.fr)
+                self.r = self.r + 0.9 * (1.0 - self.fr)
+            elif iter <= 0.9 * self.nIters:
+                self.a = self.a + 0.1 * (1.0 - self.fr) * (0.9 - float(iter) / self.nIters) / 0.3
+                self.r = self.r + 0.1 * (1.0 - self.fr) * (0.9 - float(iter) / self.nIters) / 0.3
+    
+    
